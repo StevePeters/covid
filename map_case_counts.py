235b2a8bfd2cb4c199e00d955894cb5563bed0d8
Steve Peters <steve.peters@communities.gov.uk>
2020-04-03 16:55:48 +0100

    Code tweak and images resized to A4 landscape

--- map_case_counts.py
+++ map_case_counts.py
@@ -8,7 +8,6 @@ Created on Sun Mar 29 15:49:03 2020
 
 import pandas as pd
 import geopandas as gpd
-import mapclassify as mc
 import requests
 from io import StringIO
 from datetime import datetime
@@ -26,7 +25,8 @@ def drawChart( gPos, ctitle, setForMap ):
     
   
 # load UTLA boundaries
-utla_geom_url = "/Users/stevepeters/Documents/___Steve Peters/__COVID/utla_regions.gpkg"
+latestDate=datetime.strptime('2020-03-09', "%Y-%m-%d")
+utla_geom_url = "/Users/stevepeters/Documents/GitHub/covid/data/utla_regions.gpkg"
 utla_geom_data = gpd.read_file(utla_geom_url)
 
 # load latest case counts
@@ -71,67 +71,68 @@ for td in dict_date_range:
     
     # filter case list by dates
     tdd=datetime.strptime(td, "%Y-%m-%d")
-    tdt = tdd.strftime("%d %b %Y")
-    dfilter=df['Date'] == td
-    thisDateDF=(df[dfilter])
-    setForMap = utla_geom_data.set_index('AreaCode').join(thisDateDF.set_index('AreaCode'))
-    
-    fig = plt.figure(constrained_layout=False,figsize=[50,10])
-    fig.suptitle('COVID-19 Cases in English Upper Tier LAs, by region: ' + tdt, fontdict=fontMainhead, x=0.47)
-    
-    grid = fig.add_gridspec(ncols=5, nrows=4, figure=fig, wspace=0.2, hspace=0.1)
-    ax_eng = fig.add_subplot(grid[:3, :2])
-    ax_eng.axis('off')
-    setForMap.plot(column='cumCases', cmap=cMap, linewidth=0.2, ax=ax_eng, vmin=vmin, vmax=vmax, edgecolor='0.8')
-
-    
-    # create figure and axes for Matplotlib
-    fontSubhead = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 10,
-    }
-    ax_eng.set_title('England - ' ,fontdict=fontSubhead)
-    
-    # Create colorbar as a legend
-    sm = plt.cm.ScalarMappable(cmap=cMap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm._A = []
-    divider = make_axes_locatable(ax_eng)
-    cax1 = divider.append_axes("left", size="2%", pad=0.05)
-    fig.colorbar(sm, cax=cax1, orientation="vertical")   
-    regList={}
-    setForMap.set_index('RGN19CD')
-    rowCounter=0
-    colCounter=2
-    
-    for region in dict_regions:
-       
-        rfilter=setForMap['RGN19CD'] == region
-        trset=(setForMap[rfilter])
-        trname=trset['RGN19NM'][0]
-        if trname=='Yorkshire and The Humber':
-            trname='Yorks & Humbs'
-        regList[region]={}
-        regList[region]['title']=trname
-        regList[region]['set']=trset
-        regList[region]['gpos']=grid[rowCounter, colCounter]
-        if colCounter==4:
-            rowCounter=rowCounter+1
-            colCounter=2
-        else:
-            colCounter=colCounter+1
-            
-    for region in regList:
-        drawChart( regList[region]['gpos'], regList[region]['title'], regList[region]['set'] )
+    if tdd>=latestDate:    
+        tdt = tdd.strftime("%d %b %Y")
+        dfilter=df['Date'] == td
+        thisDateDF=(df[dfilter])
+        setForMap = utla_geom_data.set_index('AreaCode').join(thisDateDF.set_index('AreaCode'))
         
-    fig.text( 0.21,0.93,'Source: Public Health England')
-    fig.text( 0.73,0.93,'Made by @SemanticSteve')
-    fig.subplots_adjust(top=0.7)
-    grid.tight_layout(fig, rect=[0.2, 0.2, 0.9, 0.9])
-    fig.subplots_adjust(top=0.7)
-    fig.set_size_inches(w=11,h=7)
+        fig = plt.figure(constrained_layout=False,figsize=[50,10])
+        fig.suptitle('COVID-19 Cases in English Upper Tier LAs, by region: ' + tdt, fontdict=fontMainhead, x=0.47)
+        
+        grid = fig.add_gridspec(ncols=5, nrows=4, figure=fig, wspace=0.2, hspace=0.1)
+        ax_eng = fig.add_subplot(grid[:3, :2])
+        ax_eng.axis('off')
+        setForMap.plot(column='cumCases', cmap=cMap, linewidth=0.2, ax=ax_eng, vmin=vmin, vmax=vmax, edgecolor='0.8')
     
-    fig_name = "img/" + td + '_utla_covid_cases.png'
-    fig.savefig(fig_name,  dpi=300)
-    plt.close(fig)
+        
+        # create figure and axes for Matplotlib
+        fontSubhead = {'family': 'serif',
+            'color':  'darkred',
+            'weight': 'normal',
+            'size': 10,
+        }
+        ax_eng.set_title('England - ' ,fontdict=fontSubhead)
+        
+        # Create colorbar as a legend
+        sm = plt.cm.ScalarMappable(cmap=cMap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+        sm._A = []
+        divider = make_axes_locatable(ax_eng)
+        cax1 = divider.append_axes("left", size="2%", pad=0.05)
+        fig.colorbar(sm, cax=cax1, orientation="vertical")   
+        regList={}
+        setForMap.set_index('RGN19CD')
+        rowCounter=0
+        colCounter=2
+        
+        for region in dict_regions:
+           
+            rfilter=setForMap['RGN19CD'] == region
+            trset=(setForMap[rfilter])
+            trname=trset['RGN19NM'][0]
+            if trname=='Yorkshire and The Humber':
+                trname='Yorks & Humbs'
+            regList[region]={}
+            regList[region]['title']=trname
+            regList[region]['set']=trset
+            regList[region]['gpos']=grid[rowCounter, colCounter]
+            if colCounter==4:
+                rowCounter=rowCounter+1
+                colCounter=2
+            else:
+                colCounter=colCounter+1
+                
+        for region in regList:
+            drawChart( regList[region]['gpos'], regList[region]['title'], regList[region]['set'] )
+            
+        fig.text( 0.21,0.93,'Source: Public Health England')
+        fig.text( 0.73,0.93,'Made by @SemanticSteve')
+        fig.subplots_adjust(top=0.7)
+        grid.tight_layout(fig, rect=[0.2, 0.2, 0.9, 0.9])
+        fig.subplots_adjust(top=0.7)
+        fig.set_size_inches(w=11.69,h=8.27)
+        
+        fig_name = "img/" + td + '_utla_covid_cases.png'
+        fig.savefig(fig_name, bbox_inches='tight', dpi=300)
+        plt.close(fig)
             
