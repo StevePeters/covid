080f778df733f88abf39ae8cbb9b4bc238690545
Steve Peters <steve.peters@communities.gov.uk>
2020-04-04 17:19:07 +0100

    Maps for 4 April, with new cases per 100k population

--- map_case_counts_mid_year_population.py
+++ map_case_counts_mid_year_population.py
@@ -8,6 +8,8 @@ Created on Sun Mar 29 15:49:03 2020
 
 import pandas as pd
 import geopandas as gpd
+import geoplot
+import mapclassify
 import requests
 from io import StringIO
 from datetime import datetime
@@ -20,26 +22,31 @@ def drawChart( gPos, ctitle, setForMap ):
     
     ax=fig.add_subplot(gPos)
     ax.axis('off')
-    tmax=setForMap['cumCases'].max()
-    setForMap.plot(column='cumCases', cmap=cMap, linewidth=0.2, ax=ax, vmin=vmin, vmax=vmax, edgecolor='0.8')
+    tmax=setForMap['popRatio'].max()
+    setForMap.plot(column='popRatio', cmap=cMap, linewidth=0.2, ax=ax, vmin=vmin, vmax=vmax, edgecolor='0.8')
     ax.set_title(ctitle,fontdict=fontSubhead)
     for idx, row in setForMap.iterrows():
-        if row.cumCases == tmax:
-            ax.text(row.coords[0]+0.2, row.coords[1], s=row['Area'], horizontalalignment='right', fontsize=3, bbox={'facecolor': 'white', 'alpha':0.5, 'pad': 1, 'edgecolor':'none'})
-    
+            if row.popRatio == tmax:
+                ax.text(row.coords[0]+0.2, row.coords[1], s=row['Area'], horizontalalignment='right', fontsize=3, bbox={'facecolor': 'white', 'alpha':0.5, 'pad': 1, 'edgecolor':'none'})
     
   
 # load UTLA boundaries
 latestDate=datetime.strptime('2020-03-09', "%Y-%m-%d")
-utla_geom_url = "/Users/stevepeters/Documents/GitHub/covid/data/utla_regions.gpkg"
+utla_geom_url = "data/utla_regions.gpkg"
 utla_geom_data = gpd.read_file(utla_geom_url)
 utla_geom_data['coords'] = utla_geom_data['geometry'].apply(lambda x: x.representative_point().coords[:])
 utla_geom_data['coords'] = [coords[0] for coords in utla_geom_data['coords']]
+
+
+# load 2018 mid year population estimates
+df_mypops = pd.read_csv("data/2018_mid_year_population_estimates.csv") 
+df_mypops["MYE_2018_total"] = df_mypops["MYE_2018_total"].apply(pd.to_numeric,errors='coerce')
 
 # load latest case counts
 utla_case_counts_url="https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv"
 s=requests.get(utla_case_counts_url).text
 df=pd.read_csv(StringIO(s),parse_dates=True)
+
 
 # fix data type errors
 df.drop(df.loc[df['Country']=="Scotland"].index, inplace=True)
@@ -60,19 +67,24 @@ df.set_index(['AreaCode', 'Date'], inplace=True)
 df['cumCases'] = cumsums
 df.reset_index(inplace=True)
 
+# join to population estimates to get max value
+df_pr=df.set_index('AreaCode').join(df_mypops.set_index('AreaCode'))
+df_pr.reset_index(inplace=True)
+df_pr['popRatio']=((df_pr.cumCases/df_pr.MYE_2018_total) * 100000)
+
+
 #  get unique list of dates
-dict_date_range=df.Date.unique()
+dict_date_range=df_pr.Date.unique()
 dict_regions=utla_geom_data.RGN19CD.unique()
 
 # set up map parameters
 fontMainhead = {'family': 'serif',
-    'color':  'darkblue',
+    'color':  'darkred',
     'weight': 'bold',
     'size': 12,
 }
-cMap='YlGnBu'
+cMap='OrRd'
 vmin= 0
-vmax=df["cumCases"].max()
 
 for td in dict_date_range:
     
@@ -80,22 +92,34 @@ for td in dict_date_range:
     tdd=datetime.strptime(td, "%Y-%m-%d")
     if tdd>=latestDate:    
         tdt = tdd.strftime("%d %b %Y")
-        dfilter=df['Date'] == td
-        thisDateDF=(df[dfilter])
+        dfilter=df_pr['Date'] == td
+        thisDateDF=(df_pr[dfilter])
         setForMap = utla_geom_data.set_index('AreaCode').join(thisDateDF.set_index('AreaCode'))
-        
+    
+        vmax=setForMap["popRatio"].max()
+
         fig = plt.figure(constrained_layout=False,figsize=[50,10])
-        fig.suptitle('COVID-19 cumulative cases since 9 Mar 20 in English Upper Tier LAs: ' + tdt, fontdict=fontMainhead, x=0.506)
+        fig.suptitle('COVID-19 daily cases per 100,000 people in English Upper Tier LAs: ' + tdt, fontdict=fontMainhead, x=0.505)
         
         grid = fig.add_gridspec(ncols=5, nrows=4, figure=fig, wspace=0.2, hspace=0.1)
         ax_eng = fig.add_subplot(grid[:3, :2])
         ax_eng.axis('off')
-        setForMap.plot(column='cumCases', cmap=cMap, linewidth=0.2, ax=ax_eng, vmin=vmin, vmax=vmax, edgecolor='0.8')
-    
         
+        # set up map classifier
+        npopr=setForMap['popRatio'].fillna(0)
+        scheme=mapclassify.NaturalBreaks(npopr, k=5)
+        #geoplot.choropleth(
+        #    setForMap, hue=npopr, scheme=scheme,
+        #    cmap='Greens',ax=ax_eng
+        #)
+        
+        setForMap.plot(column='popRatio', cmap=cMap, linewidth=0.2, ax=ax_eng, vmin=vmin, vmax=vmax, edgecolor='0.8')
+        
+        
+
         # create figure and axes for Matplotlib
         fontSubhead = {'family': 'serif',
-            'color':  'darkblue',
+            'color':  'darkred',
             'weight': 'normal',
             'size': 10,
         }
@@ -107,6 +131,9 @@ for td in dict_date_range:
         divider = make_axes_locatable(ax_eng)
         cax1 = divider.append_axes("left", size="2%", pad=0.05)
         fig.colorbar(sm, cax=cax1, orientation="vertical")   
+ 
+        
+ 
         regList={}
         setForMap.set_index('RGN19CD')
         rowCounter=0
@@ -132,14 +159,14 @@ for td in dict_date_range:
         for region in regList:
             drawChart( regList[region]['gpos'], regList[region]['title'], regList[region]['set'] )
             
-        fig.text( 0.205,0.93,'Source: Public Health England', fontsize=8)
+        fig.text( 0.205,0.93,'Source: Public Health England, ONS 2018 mid-year population estimates',  fontsize=8)
         fig.text( 0.76,0.93,'Made by @SemanticSteve', fontsize=8)
         fig.subplots_adjust(top=0.7)
         grid.tight_layout(fig, rect=[0.2, 0.2, 0.9, 0.9])
         fig.subplots_adjust(top=0.7)
         fig.set_size_inches(w=11.69,h=8.27)
         
-        fig_name = "img_casecounts/" + td + '_utla_covid_cases.png'
+        fig_name = "img_per100k/" + td + '_utla_covid_pop_ratios.png'
         fig.savefig(fig_name, bbox_inches='tight', dpi=300)
         plt.close(fig)
             
---
+++ runmaps.sh
@@ -0,0 +1,4 @@
+source activate COVID
+python /Users/stevepeters/Documents/GitHub/covid/map_case_counts.py
+python /Users/stevepeters/Documents/GitHub/covid/map_case_counts_mid_year_population.py
+exit
