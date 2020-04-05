605838150d4b4ca66ae8e253e35adac2ac915c3a
Steve Peters <steve.peters@communities.gov.uk>
2020-04-05 16:51:33 +0100

    Delete runmaps.sh

--- runmaps.sh
+++
@@ -1,4 +0,0 @@
-source activate COVID
-python /Users/stevepeters/Documents/GitHub/covid/map_case_counts.py
-python /Users/stevepeters/Documents/GitHub/covid/map_case_counts_mid_year_population.py
-exit
